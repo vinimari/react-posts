7a6d406d940e7e6fd91bc680a991865e5472db1a
vinimari <viniciusmarinho44@hotmail.com>
2025-03-04 22:43:29 -0300

    feat: add dynamic text and author

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+import NewPost from "./NewPost";
 import Post from "./Post";
-import styles from "./PostsList.module.css"
+import styles from "./PostsList.module.css";
 
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function changeBodyHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeAuthorHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
-    <ul className={styles.posts}>
-      <Post author="João" body="Texto do João" />
-      <Post author="Maria" body="Texto da Maria" />
-    </ul>
+    <>
+      <NewPost
+        onBodyChange={changeBodyHandler}
+        onAuthorChange={changeAuthorHandler}
+      />
+      <ul className={styles.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Maria" body="Texto da Maria" />
+      </ul>
+    </>
   );
 }
 
