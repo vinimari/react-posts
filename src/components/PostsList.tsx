d709259866443e7612ba6508d22ab12cf345d398
vinimari <viniciusmarinho44@hotmail.com>
2025-03-15 15:32:02 -0300

    feat: add loading state

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NewPost, { PostData } from "./NewPost";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
@@ -6,8 +6,20 @@ import Modal from "./Modal";
 import MainHeader from "./MainHeader";
 
 function PostsList() {
-  const [modalVisible, setModalVisible] = useState(false);
   const [posts, setPosts] = useState<PostData[]>([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function fetchPost() {
+      setIsFetching(true);
+      const res = await fetch("http://localhost:8080/posts");
+      const resData = await res.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+    fetchPost();
+  }, []);
 
   function hideModalHandler() {
     setModalVisible(false);
@@ -17,7 +29,14 @@ function PostsList() {
     setModalVisible(true);
   }
 
-  function addPostHandler(postData: any) {
+  function addPostHandler(postData: PostData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
@@ -35,7 +54,7 @@ function PostsList() {
     <>
       <MainHeader onCreatePost={showModalHandler} />
       {modalContent}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={styles.posts}>
           {posts.map((post) => (
             <Post
@@ -46,10 +65,15 @@ function PostsList() {
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
-        <div style={{ textAlign: "center", color: "white" }}>
+      {!isFetching && posts.length === 0 && (
+        <div className={styles.text}>
           <h2>There are no posts yet :(</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div className={styles.text}>
+          <p>Loading...</p>{" "}
         </div>
       )}
     </>
